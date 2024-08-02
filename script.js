38d1c84364a98035478d6f6fa18aec57970fc7e7
AnônimoDoRecanto <79431413+manfrimdev@users.noreply.github.com>
2024-08-02 16:42:45 -0300

    Add files via upload

---
+++ script.js
@@ -0,0 +1,19 @@
+console.log("meu primeiro script")
+let nome = "Fellipe"
+let idade = 16
+let ativo = true
+let frutas = ["maçã", "banana", "laranja"]
+let pessoa = {
+    nome: "João",
+    idade: 30,
+    ativo: false}
+    console.log("Nome:", nome);
+    console.log("Idade:", idade);
+    console.log("Ativo:", ativo);
+    console.log("Frutas:", frutas);
+    console.log("Pessoa:", pessoa);
+
+    function somarNumeros(num1, num2) { let soma = num1 + num2; return soma;
+    }
+    let resultado = somarNumeros(20, 10);
+console.log("A soma é:", resultado);
